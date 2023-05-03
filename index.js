ecab59957d8fa7d51f184ff3ae00f59ea290ba7f
bkoubik88 <benjamin.koubik@gmx.de>
2023-05-03 10:32:17 +0200

    added mongo atlas! 11

--- index.js
+++ index.js
@@ -13,17 +13,13 @@ app.use(cors());
 
 app.use("/server/user", userRoute);
 
-const db = () => {
-  mongoose
-    .connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("MONGO Runs!");
-    });
-};
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO Runs!");
+  });
 
-app.listen(3999, () => {
-  db();
-});
+app.listen(3999);
