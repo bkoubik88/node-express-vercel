41afa4497a4074e7fada47a02bcdafaa81c7bc83
bkoubik88 <benjamin.koubik@gmx.de>
2023-05-03 09:00:06 +0200

    added vercel.json 19

--- components/User.js
+++ components/User.js
@@ -4,14 +4,12 @@ import User from "../model/User.js";
 export const newUser = async (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   try {
-    if (req.body) {
-      console.log(req.body.user);
-      const user = new User({ ...req.body.user });
+    console.log(req.body.user);
+    const user = new User({ ...req.body.user });
 
-      const savedUser = await user.save();
+    const savedUser = await user.save();
 
-      res.status(200).json(savedUser);
-    }
+    res.status(200).json(savedUser);
   } catch (error) {
     console.log(error);
   }
