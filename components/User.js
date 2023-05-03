0323edb739ef12c1370448fa0adffd1512330a74
bkoubik88 <benjamin.koubik@gmx.de>
2023-05-03 09:05:10 +0200

    added vercel.json 20

--- components/User.js
+++ components/User.js
@@ -2,13 +2,11 @@ import User from "../model/User.js";
 
 //CREATE NEW USER
 export const newUser = async (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
   try {
-    console.log(req.body.user);
     const user = new User({ ...req.body.user });
-
+    console.log("SAVED USER IN MONGODB: " + user);
     const savedUser = await user.save();
-
+    res.header("Access-Control-Allow-Origin", "*");
     res.status(200).json(savedUser);
   } catch (error) {
     console.log(error);
