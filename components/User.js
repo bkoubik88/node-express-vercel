daa6df073dcc303d33603b66c7c7c1516ce9de6b
bkoubik88 <benjamin.koubik@gmx.de>
2023-05-03 11:17:44 +0200

    added mongo atlas! 21

--- components/User.js
+++ components/User.js
@@ -7,7 +7,7 @@ export const newUser = async (req, res, next) => {
 
     const savedUser = await user.save();
 
-    res.status(200).json(savedUser);
+    return res.status(200).json(savedUser);
   } catch (error) {
     console.log(error);
   }
