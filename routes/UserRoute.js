d4d88261ce66fb454d7cfa4810236efdafa1aa59
bkoubik88 <benjamin.koubik@gmx.de>
2023-05-04 09:05:07 +0200

    added savedUser 58

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,8 @@
 import express from "express";
-import { newUser, getUser } from "../components/User.js";
+import { newUser } from "../components/User.js";
 
 const router = express.Router();
 
 router.post("/new", newUser);
-router.get("/", getUser);
 
 export default router;
